fd700f8406614f3d7eac1f09ff78da803391ba9d
Moeez Rajpoot <99912138+Moeez-Rajpoot@users.noreply.github.com>
2023-07-26 17:03:34 +0500

    Updated

--- logic.js
+++ logic.js
@@ -1,14 +1,41 @@
 let toastbox = document.getElementById('toastbox');
 
+let mail = document.getElementById('a');
+let user = document.getElementById('b')
+let pass = document.getElementById('c')
+let repass = document.getElementById('d')
+
 
 function toast() {
-    let toast = document.createElement("div");
-    toast.innerHTML =' <i class="fa-solid fa-circle-check"></i> Successfully Submitted ';
-    toast.classList.add('toast');
-    toastbox.appendChild(toast);
 
-    setTimeout(()=>{
-        toast.remove();
-    },5000);
+   
 
+    if (mail.value === "" && user.value === "" && pass.value === "" && repass.value === "" )  {
+
+        let toast = document.createElement("div");
+        toast.innerHTML =' <i class="fa-sharp fa-solid fa-circle-xmark"></i> UnSuccussfull Submission ';
+        toast.classList.add('error');
+
+        toastbox.appendChild(toast);
+    
+        setTimeout(()=>{
+            toast.remove();
+        },5000);
+
+        
+    }
+
+    else{
+        let toast = document.createElement("div");
+        toast.innerHTML =' <i class="fa-solid fa-circle-check"></i> Successfully Submitted ';
+        toast.classList.add('toast');
+        toastbox.appendChild(toast);
+    
+        setTimeout(()=>{
+            toast.remove();
+        },5000);
+    
+
+    }
+   
 }
